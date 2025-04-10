76d5b87d68906e7819bce0f2f2afbd126a3bb7c7
Ray <ray@syroot.com>
2025-04-10 15:22:49 +0200

    convert.sh: Update to use croctool

--- convert.sh
+++ convert.sh
@@ -1,49 +1,48 @@
 #!/bin/bash -e
 
-MAPTOOL=./cmake-build-debug/maptool/maptool
+MAPTOOL=./cmake-build-debug/croctool/croctool
 
 mkdir -p resources/maps/{json,map,wad}/{beta012,pc,sles00593,slps01055,slus00530}
 
 # Unwad everything first
 for i in resources/maps/wad/slus00530/*.[Ww][Aa][Dd]; do
-    ${MAPTOOL} unwad $i resources/maps/wad/slus00530/$(echo "$i" | perl -pe 's/.+MAP(\d+)\.[wW][aA][dD]$/MP0\1_/g');
+    ${MAPTOOL} map unwad $i resources/maps/wad/slus00530/$(echo "$i" | perl -pe 's/.+MAP(\d+)\.[wW][aA][dD]$/MP0\1_/g');
     if [ $? -ne 0 ]; then
         echo "> $i failed"
     fi
 done
 
 for i in resources/maps/map/beta012/*.[Mm][Aa][Pp]; do
-	${MAPTOOL} decompile "$i" resources/maps/json/beta012/"$(basename "$i" .MAP).json";
+	${MAPTOOL} map decompile "$i" resources/maps/json/beta012/"$(basename "$i" .MAP).json";
     if [ $? -ne 0 ]; then
         echo "> $i failed"
     fi
 done
 
 for i in resources/maps/map/sles00593/*.[Mm][Aa][Pp]; do
-	${MAPTOOL} decompile "$i" resources/maps/json/sles00593/"$(basename "$i" .MAP).json";
+	${MAPTOOL} map decompile "$i" resources/maps/json/sles00593/"$(basename "$i" .MAP).json";
     if [ $? -ne 0 ]; then
         echo "> $i failed"
     fi
 done
 
 for i in resources/maps/map/slps01055/*.[Mm][Aa][Pp]; do
-	${MAPTOOL} decompile "$i" resources/maps/json/slps01055/"$(basename "$i" .MAP).json";
+	${MAPTOOL} map decompile "$i" resources/maps/json/slps01055/"$(basename "$i" .MAP).json";
     if [ $? -ne 0 ]; then
         echo "> $i failed"
     fi
 done
 
 for i in resources/maps/map/slus00530/*.[Mm][Aa][Pp]; do
-	${MAPTOOL} decompile "$i" resources/maps/json/slus00530/"$(basename "$i" .MAP).json";
+	${MAPTOOL} map decompile "$i" resources/maps/json/slus00530/"$(basename "$i" .MAP).json";
     if [ $? -ne 0 ]; then
         echo "> $i failed"
     fi
 done
 
 for i in resources/maps/map/pc/*.[Mm][Aa][Pp]; do
-    ${MAPTOOL} decompile "$i" resources/maps/json/pc/"$(basename "$i" .MAP).json";
+    ${MAPTOOL} map decompile "$i" resources/maps/json/pc/"$(basename "$i" .MAP).json";
 	if [ $? -ne 0 ]; then
 		echo "> $i failed"
 	fi
 done
-
